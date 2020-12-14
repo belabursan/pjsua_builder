c418b7db6d85d7580dd8ab923ae3ba01ce388a06
Bela Bursan <bela.bursan@svep.se>
2020-12-14 16:58:18 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# PJSIP and PJSUA builder for Android
+This repo contains a couple of scripts that downloads all neccessary components and builds the PJSIP/PJSUA lib for Android
+
+## Step 1
+Work in a docker. Open Windows power shell and execute the following commands:
+ - docker pull ubuntu:latest
+ - docker run -v [path_to_directory_in_your_home_directory]:/home/bin  -it ubuntu bash
+
+ Copy the prepare_docker.sh to that directory.
+
+## Step 2
+In the console, go to /home/bin and execute the shell script:
+ - ./docker_prepare.sh
+ (Sometimes you have to set the right permissions: chmod +x docker_prepare.sh)
+
+
+## Step 3
+Got to "pjsua-builder" directory and run ./configure
+It is possible to run ./configure X to open and edit the config file
+When done, run ./build
+
+It shall result in a libpjsua.zip file in the working directory.
+
+
+## Versions
+Openssl    : 1.1.1h
+Openh264   : 2.1.1
+Swig       : 4.0.2
+opus       : 1.3.1
+Pjsip      : 2.10
+Android ndk: r21d
+
+It is possible to set versions in the scripts/config file, open by running ./configure x
---
+++ docker_prepare.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+# Script for preparing a docker to be able to compile the pjsip-android library
+# 2020 Bela Bursan
+# v0.0.6
+set -e
+
+#docker pull ubuntu:latest
+#docker run -v C:/Users/bub/Desktop/SVEP/work/volume:/home/bin  -it ubuntu bash
+
+WORKDIR="pjsua-builder"
+NEWGIT="1"
+GIT_BRANCH=""
+
+## Colors
+C="\033[96m"  # Cyan
+R="\033[91m"  # Red
+E="\033[0m"   # End of color
+Y="\033[93m"  # Yellow color
+
+echo "$C\n Preparing docker to be able tobuild PJSIP$E\n"
+if [ $(pwd) != "/home/bin" ]; then
+    echo "$R\n -Wrong directory, change to /home/bin!!\n$E"
+    exit 1
+fi
+
+## Install some needed apps
+echo "$C -Updating$E"
+apt-get -o Acquire::Check-Valid-Until=false -o Acquire::Check-Date=false update
+echo "$C -Installing nano$E"
+apt install -y nano
+sed -i 's/# set linenumbers/set linenumbers/g' /etc/nanorc
+
+echo "$C -Installing git$E"
+apt install -y git
+
+## Add  user to sudoers
+echo "$C Installing sudo$E"
+apt-get install sudo
+usermod -a -G sudo root
+
+## prepare aliases
+echo "$C -Creating aliases$E"
+echo "####### A L I A S E S ####### \
+\n## \
+\nalias ..=\"cd ..\" \
+\nalias ...=\"cd ../..\" \
+\n## \
+\nalias nn=\"nano\" \
+\nalias l=\"ls -lh\" \
+\nalias ll=\"ls -lAh\" \
+\n## \
+\nalias grepi=\"grep -irnI\" \
+\nalias findi=\"find . -name\"\n" > ~/.bash_aliases
+echo "$Y !DON'T FORGET TO RUN: source ~/.bashrc$E"
+
+
+## prepare workdir
+if [ "$NEWGIT" != "" ]; then
+    if [ -d "$WORKDIR" ]; then
+        echo "$C Removing old working directory: $WORKDIR $E"
+        rm -rf "$WORKDIR"
+    fi
+fi
+
+## clone git
+if [ -d "$WORKDIR" ]; then
+    echo "$Y -Git already cloned$E"
+else
+    echo "$C -Cloning git repo$E"
+    git clone https://github.com/belabursan/pjsip-android-builder.git
+fi
+
+cd "$WORKDIR"
+if [ "$GIT_BRANCH" != "" ]; then
+    echo "$Y Checking out branch: $GIT_BRANCH$E"
+    git checkout "$GIT_BRANCH"
+fi
+git pull
+
+# Fix file permissions
+cd "$BASEDIR/scripts"
+chmod +x *
+
+echo "\n\n   ! Now run \e[41msource ~/.bashrc\e[49m and after that got to $WORKDIR and run  \e[41m./prepare-build-system \e[49m\n\n"
